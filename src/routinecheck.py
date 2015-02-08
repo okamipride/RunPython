bc42d29466b836f094d58991f1b404788104cb61
okamipride <pieceofr@gmail.com>
2015-02-08 18:55:59 +0800

    modify write file option in py

--- src/routinecheck.py
+++ src/routinecheck.py
@@ -1,14 +1,20 @@
 import time
+import os.path
 
 print("start time routine check") 
 
 runcheck = True
 # Check every checksec
-checksec = 10
-
+checksec = 5
+#filepath = "F:/Python/workspace/files/routinefilejava.txt"
+filepath = "routinefilejava.txt"
 while runcheck:  
     #f = open('F:/Python/workspace/files/routinefilejava.txt', 'w')
-    f = open('routinefilejava.txt', 'a')
+    fexist = os.path.isfile(filepath)
+    if fexist :
+        f = open(filepath, 'a')
+    else:
+        f = open(filepath, 'w')
     f.write('hi there\n') # python will convert \n to os.linesep
     f.close() # you can omit in most cases as the destructor will call if
     time.sleep(checksec)
